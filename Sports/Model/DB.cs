41e841cece9fcd2c82c61d8944b073230b63f870
abdullahmahmoud560 <yabbghagh@gmail.com>
2025-06-29 22:36:48 +0300

    change name table

--- Sports/Model/DB.cs
+++ Sports/Model/DB.cs
@@ -11,11 +11,16 @@ namespace Sports.Model
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
             modelBuilder.Entity<Academy>().HasIndex(u => u.AcademyEmail).IsUnique();
+            modelBuilder.Entity<Academy>().ToTable("academies");
+            modelBuilder.Entity<Role>().ToTable("roles");
+            modelBuilder.Entity<Match>().ToTable("matches");
+            modelBuilder.Entity<Player>().ToTable("players");
+            modelBuilder.Entity<Camp>().ToTable("camps");
         }
-        public DbSet<Role> Roles { get; set; }
-        public DbSet<Academy> Academies { get; set; }
-        public DbSet<Match> Matches { get; set; }
-        public DbSet<Player> Players { get; set; }
+        public DbSet<Role> roles { get; set; }
+        public DbSet<Academy> academies { get; set; }
+        public DbSet<Match> matches { get; set; }
+        public DbSet<Player> players { get; set; }
         public DbSet<Camp> camps { get; set; }
     }
 }
